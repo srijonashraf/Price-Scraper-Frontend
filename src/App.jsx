2b3be75ae8a0a719e3befbaf3c5e084d95698bee
Srijon Ashraf <72869452+srijonashraf@users.noreply.github.com>
2024-01-08 01:22:50 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,136 @@
+import React, { useState, useEffect } from 'react';
+import Search from './Search';
+import Loading from 'react-fullscreen-loading';
+import { Toaster, toast } from 'react-hot-toast';
+
+const App = () => {
+  const [results, setResults] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [minPrice, setMinPrice] = useState(null);
+  const [siteImageMap, setSiteImageMap] = useState({});
+
+  const BaseURL = 'https://price-scrapper-backend.onrender.com/api/v1';
+
+  const handleSearch = async (keyword) => {
+    try {
+      setLoading(true);
+      const response = await fetch(`${BaseURL}/scrape/${encodeURIComponent(keyword)}`);
+
+      if (!response.ok) {
+        throw new Error('Failed to fetch data');
+      }
+
+      const data = await response.json();
+
+      if (data.data) {
+        const { minPriceSource, ...filteredResults } = data.data;
+
+        toast.success('Data fetched successfully!');
+        setResults(filteredResults);
+        setMinPrice(minPriceSource);
+      } else {
+        setResults(null);
+      }
+
+      setLoading(false);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      setResults(null);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    const fetchLogoData = async () => {
+      try {
+        const response = await fetch('/logo.json');
+        if (!response.ok) {
+          throw new Error('Failed to fetch logo data');
+        }
+
+        const logoData = await response.json();
+        const newSiteImageMap = {};
+        logoData.forEach(({ name, img }) => {
+          newSiteImageMap[name] = img;
+        });
+
+        setSiteImageMap(newSiteImageMap);
+      } catch (error) {
+        console.error('Error fetching logo data:', error);
+      }
+    };
+
+    fetchLogoData();
+  }, []); // Empty dependency array to run the effect only once
+
+  const LogoMarquee = () => (
+    <marquee className="d-flex flex-row">
+      {Object.entries(siteImageMap).map(([siteName, imageUrl]) => (
+        <img
+          key={siteName}
+          style={{ height: '40px', width: 'auto', objectFit: 'contain', backgroundColor: '#343a40' }}
+          src={imageUrl}
+          alt={siteName}
+          className="mx-1 rounded-1 img-fluid img-thumbnail"
+        />
+      ))}
+    </marquee>
+  );
+
+  return (
+    <div>
+      <Toaster position="top-right" />
+      <div className="container">
+        <h1 className="text-center mt-4">Price Scraper</h1>
+        <LogoMarquee />
+        <Search onSearch={handleSearch} />
+        {loading && <Loading loading loaderColor="#3498db" />}
+        {loading && <p className="text-center mt-2 fs-2">Fetching all the deals...</p>}
+        {results && !loading && (
+          <div className="container mt-4">
+            <div className="row justify-content-center">
+              {Object.entries(results).map(([source, result]) => (
+                <div key={source} className="col-md-4 mb-4">
+                  <div className={`card h-100 shadow ${minPrice === result.siteName ? 'border-primary border-2' : ''}`}>
+                    <div className="card-body p-4">
+                      <h5 className="card-title">{result.productName}</h5>
+                      <img
+                        style={{ height: '40px', width: 'auto', objectFit: 'contain', backgroundColor: '#343a40' }}
+                        src={siteImageMap[result.siteName]}
+                        alt={result.siteName}
+                        className="card-img-top d-flex my-2 rounded-1 img-fluid img-thumbnail"
+                      />
+                      <p className="card-text btn btn-success rounded-1 btn-sm">
+                        Price: {result.price || result.priceNew}
+                      </p>
+                      {minPrice === result.siteName && (
+                        <p className="badge rounded-1 bg-danger float-end fs-6">Lowest Price!!</p>
+                      )}
+                      <p>
+                        Availability:{' '}
+                        <span className={`card-text fw-bold ${result.availability === 'In Stock' ? 'text-success' : 'text-danger'}`}>
+                          {result.availability}
+                        </span>
+                      </p>
+                      <p className="card-text fw-semibold">From: {result.siteName}</p>
+                      {result.productUrl && (
+                        <p className="card-text card-footer">
+                          Product URL:{' '}
+                          <a href={result.productUrl} target="_blank" rel="noopener noreferrer">
+                            {result.productUrl}
+                          </a>
+                        </p>
+                      )}
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Search.jsx
@@ -0,0 +1,36 @@
+// src/Search.js
+
+import React, { useState } from 'react';
+
+const Search = ({ onSearch }) => {
+    const [keyword, setKeyword] = useState('');
+
+    const handleSearch = () => {
+        if (keyword.trim() !== '') {
+            onSearch(keyword.trim());
+        }
+    };
+
+    return (
+        <div className="container mt-5">
+            <div className="row justify-content-center">
+                <div className="col-md-6">
+                    <div className="input-group">
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Enter product keyword"
+                            value={keyword}
+                            onChange={(e) => setKeyword(e.target.value)}
+                        />
+                        <button className="btn btn-primary" onClick={handleSearch}>
+                            Search
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Search;
